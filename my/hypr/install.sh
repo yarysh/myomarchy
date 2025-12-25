da3cabe235534b7c052bfa112a2be19ef6f5f4ed
Andrey Yarysh <and@yarysh.com>
2025-12-25 14:11:52 +0100

    autostart hypr

--- my/hypr/install.sh
+++ my/hypr/install.sh
@@ -11,3 +11,15 @@ sudo pacman -S --noconfirm --needed minizip && yay -S --noconfirm --needed vicin
 # Copy configuration files
 mkdir -p ~/.config/hypr
 cp -r --backup configs/* ~/.config/hypr/
+
+# Autostart Hyprland
+cat > ~/.zprofile <<EOF
+if uwsm check may-start; then
+    exec uwsm start hyprland.desktop
+fi
+EOF
+
+# Prevent systemd-networkd-wait-online timeout on boot
+# Otherwise hypr won't start with message: graphical.target is queued for start, waiting for 60s...
+sudo systemctl disable systemd-networkd-wait-online.service
+sudo systemctl mask systemd-networkd-wait-online.service
