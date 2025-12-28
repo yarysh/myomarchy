8351db707902875819dec1699ae2df799f1707f0
Andrey Yarysh <and@yarysh.com>
2025-12-28 21:00:08 +0100

    set ghostty as a default terminal

--- my/ghostty/install.sh
+++ my/ghostty/install.sh
@@ -7,3 +7,5 @@ mkdir -p ~/.config/ghostty
 cp --backup backgrounds/* ~/.config/waypaper/backgrounds/
 
 cp --backup config.ini ~/.config/waypaper/config.ini
+
+xdg-mime default com.mitchellh.ghostty.desktop x-scheme-handler/terminal
